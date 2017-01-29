acf228cf1340767e9aaee880d0b11e6ac7b67c9f
eyece <fredrichawk@gmail.com>
2017-01-29 12:34:14 +0100

    Added ui list, api search url and inital style

---
+++ app.js
@@ -0,0 +1,2 @@
+const api = `https://en.wikipedia.org/w/api.php?action=query&format=json&prop=pageimages%7Cextracts&titles=Main+Page&generator=search&exsentences=2&exlimit=max&exintro=1&explaintext=1&gsrsearch=${searchTerm}&gsrlimit=10`;
+let searchTerm;
