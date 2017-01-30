74247b1365de7be33ea5952e7cd2159f17aa8d2e
eyece <fredrichawk@gmail.com>
2017-01-30 07:06:35 +0100

    worked on the api results from wikipedia

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
-const api = `https://en.wikipedia.org/w/api.php?action=query&format=json&prop=pageimages%7Cextracts&titles=Main+Page&generator=search&exsentences=2&exlimit=max&exintro=1&explaintext=1&gsrsearch=${searchTerm}&gsrlimit=10`;
-let searchTerm;
+const searchField = document.querySelector("#searchField");
+const search = document.querySelector(".search_btn");
+
+// const results = [];
+
+function handleSearch(e) {
+  e.preventDefault();
+  let searchTerm = searchField.value;
+  const api = `https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=pageimages%7Cextracts&titles=Main+Page&generator=search&utf8=1&exsentences=2&exlimit=max&exintro=1&explaintext=1&gsrsearch=${searchTerm}&gsrlimit=10`;
+
+  const results = [];
+
+  fetch(api)
+    .then(blob => blob.json())
+    .then(data => results.push(...data));
+    // .then(console.log(results));
+  searchField.value = '';
+  // console.log(results);
+  return results;
+}
+
+
+search.addEventListener('click', handleSearch);
