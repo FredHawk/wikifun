212ef38a58c7e5c56efa7efbe59684373b924357
eyece <fredrichawk@gmail.com>
2017-01-31 07:39:18 +0100

    fetching of data

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
 const searchField = document.querySelector("#searchField");
 const search = document.querySelector(".search_btn");
 
-// const results = [];
-
 function handleSearch(e) {
   e.preventDefault();
   let searchTerm = searchField.value;
   const api = `https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=pageimages%7Cextracts&titles=Main+Page&generator=search&utf8=1&exsentences=2&exlimit=max&exintro=1&explaintext=1&gsrsearch=${searchTerm}&gsrlimit=10`;
 
-  const results = [];
+  let results = [];
 
   fetch(api)
-    .then(blob => blob.json())
-    .then(data => results.push(...data));
-    // .then(console.log(results));
+    .then(response => response.json())
+    .then(data => Object.keys(data.query.pages).map(key => {
+      results = data.query.pages[key];
+      results.key = key;
+      return results;
+    })
+    // Populate the list with results
+  
+    // update DOM
+    )
+    .catch(error => console.error(error));
+  
   searchField.value = '';
-  // console.log(results);
-  return results;
 }
 
 
