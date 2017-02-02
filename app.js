bae2b924a3a426672957dfed7121088088c7db5c
eyece <fredrichawk@gmail.com>
2017-02-02 07:39:25 +0100

    made some adjustments with how to get array

--- app.js
+++ app.js
@@ -2,40 +2,38 @@ const searchField = document.querySelector('#searchField');
 const search = document.querySelector('.search_btn');
 const listResult = document.querySelector('.results')
 
-function showResults(results) {
-  return (`
-    <li class="result">
-      <img class="result_image" src="https://placehold.it/100x100" alt="PLaceholder">
-      <h4 class="result_title">Lorem ipsum dolor sit amet, consectetur adipisicing.</h4>
-      <p class="result_description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur architecto cum asperiores dolor delectus tempore quasi aperiam commodi? Aperiam, earum.</p>
-      <a href="#" class="result_link"></a>
-    </li>
-  `);
+function showResults(e) {
+  e.preventDefault();
+  const results = handleSearch();
+  console.log(results);
+  const html = results.map(result => {
+  //   return `
+  //   <li class="result">
+  //     <img class="result_image" src="https://placehold.it/100x100" alt="PLaceholder">
+  //     <h4 class="result_title">Lorem ipsum dolor sit amet, consectetur adipisicing.</h4>
+  //     <p class="result_description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur architecto cum asperiores dolor delectus tempore quasi aperiam commodi? Aperiam, earum.</p>
+  //     <a href="#" class="result_link"></a>
+  //   </li>
+  // `;
+  return 'yay';
+  console.log('yay');
+  });
+  console.log(results);
+  listResult.innerHTML = html;
 }
 
-function handleSearch(e) {
-  e.preventDefault();
-  let searchTerm = searchField.value;
-  const api = `https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=pageimages%7Cextracts&titles=Main+Page&generator=search&utf8=1&exsentences=2&exlimit=max&exintro=1&explaintext=1&gsrsearch=${searchTerm}&gsrlimit=10`;
-
+function handleSearch() {
   let results = [];
+  let searchTerm = searchField.value;
+  // Remember to change srsearch=green to srsearch=${searchTerm}
+  const api = `https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=pageimages%7Cextracts&titles=Main+Page&generator=search&utf8=1&exsentences=2&exlimit=max&exintro=1&explaintext=1&gsrsearch=green&gsrlimit=10`;
 
   fetch(api)
     .then(response => response.json())
-    .then(data => Object.keys(data.query.pages).map(key => {
-      results = data.query.pages[key];
-      results.key = key;
-      return results;
-    })
-    // Populate the list with results
-    .then(results => results.map(showResults))
-    // update DOM
-    // .then()
-    )
+    .then(data => Object.keys(data.query.pages).map(key => results.push(data.query.pages[key])))
     .catch(error => console.error(error));
-  
-  searchField.value = '';
+  return results;
 }
 
 
-search.addEventListener('click', handleSearch);
+search.addEventListener('click', showResults);
