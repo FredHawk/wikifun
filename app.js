1c7228e1d46b3ec4b310f973e2bd57997adbd103
eyece <fredrichawk@gmail.com>
2017-02-07 07:31:50 +0100

    solved the fetching problem

--- app.js
+++ app.js
@@ -2,52 +2,45 @@ const searchField = document.querySelector('#searchField');
 const search = document.querySelector('.search_btn');
 const listResult = document.querySelector('.results')
 
-function showResults(e) {
-  e.preventDefault();
-  const arr = [1,1,11,1,1,1,2,31243];
-  const wikiStuff = new Promise(
-    function(resolve, reject) {
-      const res = handleSearch();
-      if(res) {
-        resolve(res);
-        return res;
-      } else {
-        reject(Error("Failed!"));
-      }
-    }
-  );
-  wikiStuff.then(function(result) {
-    console.log('during promise',  result);
-    // console.log(Promise['["[[PromiseValue]]"]']);
-  });
-  console.log('after promise ', wikiStuff);
-  // console.log(wikiStuff.length);
-  console.log(wikiStuff.length);
-  const html = arr.map(result => {
-    return `
-    <li class="result">
-      <img class="result_image" src="https://placehold.it/100x100" alt="Placeholder">
-      <h4 class="result_title">Lorem ipsum dolor sit amet, consectetur adipisicing.</h4>
-      <p class="result_description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur architecto cum asperiores dolor delectus tempore quasi aperiam commodi? Aperiam, earum.</p>
-      <a href="#" class="result_link"></a>
-    </li>
-  `;
-  }).join('');
-  listResult.innerHTML = html;
+function showResults() {
+  // const html = arr.map(result => {
+  //   return `
+  //   <li class="result">
+  //     <img class="result_image" src="https://placehold.it/100x100" alt="Placeholder">
+  //     <h4 class="result_title">Lorem ipsum dolor sit amet, consectetur adipisicing.</h4>
+  //     <p class="result_description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur architecto cum asperiores dolor delectus tempore quasi aperiam commodi? Aperiam, earum.</p>
+  //     <a href="#" class="result_link"></a>
+  //   </li>
+  // `;
+  // }).join('');
+  // listResult.innerHTML = html;
 }
 
-function handleSearch() {
+function handleSearch(e) {
+  e.preventDefault();
   let results = [];
   let searchTerm = searchField.value;
   // Remember to change srsearch=green to srsearch=${searchTerm}
-  const api = `https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=pageimages%7Cextracts&titles=Main+Page&generator=search&utf8=1&exsentences=2&exlimit=max&exintro=1&explaintext=1&gsrsearch=green&gsrlimit=11`;
+  const api = `https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=pageimages%7Cextracts&titles=Main+Page&generator=search&utf8=1&exsentences=2&exlimit=max&exintro=1&explaintext=1&gsrsearch=green&gsrlimit=10`;
 
   fetch(api)
     .then(response => response.json())
-    .then(data => Object.keys(data.query.pages).map(key => results.push(data.query.pages[key])))
+    .then(data => {
+      Object.keys(data.query.pages).map(key => results.push(data.query.pages[key]))
+      console.log(results);
+      const html = results.map(result => {
+          return `
+          <li class="result">
+            <img class="result_image" src="https://placehold.it/100x100" alt="Placeholder">
+            <h4 class="result_title">${result.title}</h4>
+            <p class="result_description">${result.extract}</p>
+            <a href="#" class="result_link"></a>
+          </li>
+        `;
+        }).join('');
+        listResult.innerHTML = html;
+    })
     .catch(error => console.error(error));
-  return results;
 }
 
-
-search.addEventListener('click', showResults);
+search.addEventListener('click', handleSearch);
