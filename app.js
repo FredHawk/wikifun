7da1a1fdfe88b1968f92facfc1b30c936cb12b64
eyece <fredrichawk@gmail.com>
2017-02-14 06:40:27 +0100

    added hover, font, changed classnames, cleaned up code

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const searchField = document.querySelector('#searchField');
-const search = document.querySelector('.search_btn');
+const search = document.querySelector('.form__button');
 const listResult = document.querySelector('.results')
 
 function handleSearch(e) {
@@ -22,29 +22,28 @@ function handleSearch(e) {
     `;
   } else {
 
-  const cleanedSearchTerm = searchTerm.split(' ').join('+');
+    const cleanedSearchTerm = searchTerm.split(' ').join('+');
 
-  // Remember to change srsearch=green to srsearch=${cleanedSearchTerm}
-  const api = `https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=pageimages%7Cextracts&titles=Main+Page&generator=search&utf8=1&exsentences=2&exlimit=max&exintro=1&explaintext=1&gsrsearch=${cleanedSearchTerm}&gsrlimit=10`;
+    const api = `https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=pageimages%7Cextracts&titles=Main+Page&generator=search&utf8=1&exsentences=2&exlimit=max&exintro=1&explaintext=1&gsrsearch=${cleanedSearchTerm}&gsrlimit=10`;
 
-  fetch(api)
-    .then(response => response.json())
-    .then(data => {
-      Object.keys(data.query.pages).map(key => results.push(data.query.pages[key]))
-      // console.log(results);
-      const html = results.map(result => {
-          return `
-          <li class="result">
-            <a href="https://en.wikipedia.org/?curid=${result.pageid}" class="result_link" target="_blank">
-            <h3 class="result_title">${result.title}</h3>
-            <p class="result_description">${result.extract}</p>
-            </a>
-          </li>
-        `;
-        }).join('');
-        listResult.innerHTML = html;
-    })
-    .catch(error => console.error(error));
+    fetch(api)
+      .then(response => response.json())
+      .then(data => {
+        Object.keys(data.query.pages).map(key => results.push(data.query.pages[key]))
+        // console.log(results);
+        const html = results.map(result => {
+            return `
+            <li class="result">
+              <a href="https://en.wikipedia.org/?curid=${result.pageid}" class="result_link" target="_blank">
+              <h3 class="result_title">${result.title}</h3>
+              <p class="result_description">${result.extract}</p>
+              </a>
+            </li>
+          `;
+          }).join('');
+          listResult.innerHTML = html;
+      })
+      .catch(error => console.error(error));
   }
 }
 
